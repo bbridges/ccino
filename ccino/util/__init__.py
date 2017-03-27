9dee2378e456997504d3dd968709813c346a851f
bloof-bb <bloof.bb@gmail.com>
2017-03-27 15:23:46 -0500

    Add get_func_args() in get_func_args

--- ccino/util/__init__.py
+++ ccino/util/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 
-from .get_func_args import get_num_args
+from .get_func_args import get_func_args, get_num_args
 from .load_module import load_module
 from .make_builtin import make_builtin
--- ccino/util/get_func_args.py
+++ ccino/util/get_func_args.py
@@ -5,6 +5,29 @@ import sys
 PYTHON_3 = sys.version_info[0] == 3
 
 
+def get_func_args(func):
+    """Return the names of the arguments of a function.
+
+    This does not differentiate between positional and keyword
+    arguments.
+
+    Args:
+        func: The function to analyze.
+
+    Returns:
+        List of argument names.
+    """
+
+    if PYTHON_3:
+        signature = inspect.signature(func)
+
+        return [param for param in signature.parameters]
+    else:
+        argspec = inspect.getargspec(func)
+
+        return argspec.args
+
+
 def get_num_args(func):
     """Return the number of arguments of a function.
 
