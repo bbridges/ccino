a150fb902c3c23f24c90880a48b669113a2da077
bloof-bb <bloof.bb@gmail.com>
2017-04-27 22:10:21 -0500

    Add printing options

--- ccino/cli.py
+++ ccino/cli.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import os
 import platform
 import sys
 
@@ -9,6 +10,10 @@ from . import main_runner
 from .reporters import get_reporter_names, get_reporter_desc
 from .util import load_module
 from .version import __version__
+
+
+DEFAULT_CONFIG = 'ccino.yml'
+DEFAULT_REPORTER = 'default'
 
 
 settings = {
@@ -38,24 +43,52 @@ def print_reporters(ctx, param, value):
     ctx.exit()
 
 
-def click_exception(message):
-    click.echo(err=True)
+def check_reporter(name):
+    name = name.lower()
 
-    raise click.ClickException(message)
+    reporters = get_reporter_names()
+
+    script = sys.argv[0]
+
+    if not script.startswith('python'):
+        script = os.path.basename(script)
+
+    if not name in reporters:
+        msg = '"{}"" not found. Use "{} --reporters" to list all reporters.' \
+                .format(name, script)
+
+        raise click.BadParameter(msg, param_hint='"reporter"')
+
+    return name
 
 
 @click.command(context_settings=settings, options_metavar='[options]')
 @click.argument('files', nargs=-1, metavar='[files]',
         type=click.Path(exists=True, resolve_path=True))
-@click.option('--bail', '-b', count=True,
-        help='Stop running after a test failure.')
+@click.option('--bail', '-b', 'bail', flag_value='True',
+        help='Stop running after a test failure. TODO')
+@click.option('--no-bail', '-B', 'bail', flag_value='False',
+        help='Don\'t stop running after a test failure. TODO')
 @click.option('--reporter', '-R', metavar='<name>',
         help='Specify the reporter to use.')
+@click.option('--no-color', '-C', 'color', flag_value='False',
+        help='Force no color output. TODO')
 @click.option('--recursive', '-r', count=True,
-        help='Load in subdirectories.')
-@click.option('--config', metavar='<file>', help='Specify the config file.')
+        help='Load in subdirectories. TODO')
+@click.option('--config', metavar='<path>', type=click.Path(resolve_path=True),
+        default=DEFAULT_CONFIG, help='Specify the config file.')
 @click.option('--save', is_flag=True,
-        help='Save the current options in the calling directory.')
+        help='Save the current options in the config file. TODO')
+@click.option('--out', metavar='<file>', type=click.Path(resolve_path=True),
+        help='Save the output to a file. TODO')
+@click.option('--stdout', metavar='<file>', type=click.Path(resolve_path=True),
+        help='Save the stdout output to a file. TODO')
+@click.option('--mirror', flag_value='True',
+        help='Also print the output to the console. TODO')
+@click.option('--mirror-stdout', flag_value='True',
+        help='Also print the stdout output to the console. TODO')
+@click.option('--cover', flag_value='True',
+        help='Output coverage information using coverage.py. TODO')
 @click.option('--reporters', is_flag=True, callback=print_reporters,
         expose_value=False, is_eager=True,
         help='List available reporters and exit.')
